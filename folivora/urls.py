524b0c393526e52d30bf40a0693962da0d66e815
Florian Apolloner <florian@apolloner.eu>
2012-08-18 08:15:56 +0200

    hookup some urls.

--- folivora/urls.py
+++ folivora/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('folivora.views',
-    url('^test/$', 'test'),
-    url('^projects/$', 'project_list', name='project_list'),
+    url('^$', 'test', name='folivora_index'),
+    url('^projects/$', 'project_list', name='folivora_project_list'),
 )
