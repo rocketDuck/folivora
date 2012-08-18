4f81836f20d21dcbc8fb611fd9fd7588a0aa787d
Christopher Grebs <cg@webshox.org>
2012-08-18 05:23:30 +0200

    add packageversion model

--- folivora/core/models.py
+++ folivora/core/models.py
@@ -8,3 +8,9 @@ class Package(models.Model):
     name = models.CharField(max_length=255, unique=True)
     url = models.URLField()
     provider = models.CharField(max_length=255, choices=(('PyPi', 'pypi'),))
+
+
+class PackageVersion(models.Model):
+    package = models.ForeignKey(Package)
+    version = models.CharField(max_length=255)
+    release_date = models.DateTimeField()
--- folivora/core/tests.py
+++ folivora/core/tests.py
@@ -1,5 +1,8 @@
+import pytz
+from datetime import datetime
 from django.test import TestCase
-from folivora.core.models import Package
+from django.utils.timezone import make_aware
+from folivora.core.models import Package, PackageVersion
 
 
 class TestPackageModel(TestCase):
@@ -12,3 +15,21 @@ class TestPackageModel(TestCase):
         self.assertEqual(pkg.name, 'gunicorn')
         self.assertEqual(pkg.url, 'http://pypi.python.org/pypi/gunicorn')
         self.assertEqual(pkg.provider, 'pypi')
+
+
+class TestPackageVersionModel(TestCase):
+
+    def test_creation(self):
+        Package.objects.create(name='gunicorn',
+                               url='http://pypi.python.org/pypi/gunicorn',
+                               provider='pypi')
+        pkg = Package.objects.get(name='gunicorn')
+        PackageVersion.objects.create(package=pkg,
+                                      version='0.14.6',
+                                      release_date=datetime(2012, 7, 26, 23, 51, 18))
+        vers = PackageVersion.objects.get(package__name='gunicorn',
+                                          version='0.14.6')
+        self.assertEqual(vers.package, pkg)
+        self.assertEqual(vers.version, '0.14.6')
+        self.assertEqual(vers.release_date,
+                         make_aware(datetime(2012, 7, 26, 23, 51, 18), pytz.UTC))
