faf5cfa8e632aebf4e022f3642be59439f4ead57
Florian Apolloner <florian@apolloner.eu>
2012-08-18 08:10:31 +0200

    more templates.

--- folivora/urls.py
+++ folivora/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('folivora.views',
-    url('^test$', 'test'),
+    url('^test/$', 'test'),
+    url('^projects/$', 'project_list', name='project_list'),
 )
--- folivora/views.py
+++ folivora/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+
+from braces.views import LoginRequiredMixin
+
+from .models import Project
 
 def test(request):
     return render(request, 'base.html')
+
+
+class ProjectListView(LoginRequiredMixin, ListView):
+    def get_queryset(self):
+        return Project.objects.filter(members=self.request.user)
+
+
+project_list = ProjectListView.as_view()
