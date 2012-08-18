96db645a2fa7a9ac32e42a3470d791a7022641b4
Oskar Hahn <mail@oshahn.de>
2012-08-18 12:22:45 +0200

    added 'registration' to INSTALLED_APPS

--- example/settings.py
+++ example/settings.py
@@ -122,6 +122,7 @@ INSTALLED_APPS = (
     'djcelery',
     'gunicorn',
     'debug_toolbar',
+    'registration',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
