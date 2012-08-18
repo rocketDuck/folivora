08f99edea08e3d6813ddec774c23a451eeeb9b2e
Florian Apolloner <florian@apolloner.eu>
2012-08-18 09:16:14 +0200

    add floppyforms and initial project add view for ostcar.

--- folivora/views.py
+++ folivora/views.py
@@ -1,18 +1,32 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
 
 from braces.views import LoginRequiredMixin
 
+from .forms import AddProjectForm
 from .models import Project
 
 
 folivora_index = TemplateView.as_view(template_name='folivora/index.html')
 
 
-class ProjectListView(LoginRequiredMixin, ListView):
+class ListProjectView(LoginRequiredMixin, ListView):
+    context_object_name = 'projects'
+
     def get_queryset(self):
         return Project.objects.filter(members=self.request.user)
 
 
-project_list = ProjectListView.as_view()
+project_list = ListProjectView.as_view()
+
+
+class AddProjectView(CreateView):
+    model = Project
+    form_class = AddProjectForm
+
+
+project_add = AddProjectView.as_view()
+
+project_detail = lambda x, slug: render(x, 'folivora/index.html')
