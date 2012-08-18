783fb69996d2a52f1dd043f04dd9b2f4f6224643
Florian Apolloner <florian@apolloner.eu>
2012-08-18 08:24:53 +0200

    Added dummy start page and some other blabla.

--- folivora/urls.py
+++ folivora/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('folivora.views',
-    url('^$', 'test', name='folivora_index'),
+    url('^$', 'folivora_index', name='folivora_index'),
     url('^projects/$', 'project_list', name='folivora_project_list'),
 )
