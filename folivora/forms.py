8976140b0b1e3d0ee2eaa4f5164bf8da4eb90a12
Oskar Hahn <mail@oshahn.de>
2012-08-18 11:45:47 +0200

    validate the Jabber ID

--- folivora/forms.py
+++ folivora/forms.py
@@ -1,5 +1,20 @@
+from django import forms
+from django.utils.translation import ugettext as _
+
 from .models import Project, UserProfile
 from .utils.forms import ModelForm
+from .utils.jabber import may_be_valid_jabber
+
+
+class JabberField(forms.fields.CharField):
+    def clean(self, value):
+        if not value:
+            return
+        value = value.strip()
+        if not may_be_valid_jabber(value):
+            raise forms.ValidationError(_(u'The entered Jabber address is invalid. '
+                u'Please check your input.'))
+        return value
 
 
 class AddProjectForm(ModelForm):
@@ -9,6 +24,7 @@ class AddProjectForm(ModelForm):
 
 
 class UpdateUserProfileForm(ModelForm):
+    jabber = JabberField()
     class Meta:
         model = UserProfile
         exclude = ('user',)
---
+++ folivora/utils/jabber.py
@@ -0,0 +1,12 @@
+import re
+from django.conf import settings
+
+
+# NOTE: according to rfc4622 a nodeid is optional. But we require one
+# 'cause nobody should enter a service-jid in the jabber field.
+# That way we don't need to validate the domain and resid.
+_jabber_re = re.compile(r'(?xi)(?:[a-z0-9!$\(\)*+,;=\[\\\]\^`{|}\-._~]+)@')
+
+
+def may_be_valid_jabber(jabber):
+    return _jabber_re.match(jabber) is not None
