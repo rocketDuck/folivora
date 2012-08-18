befc344260fca90dccc5a660b314e00f1eaca52e
Christopher Grebs <cg@webshox.org>
2012-08-18 07:29:20 +0200

    South integration, initial sync once models are flashed out

--- example/settings.py
+++ example/settings.py
@@ -111,6 +111,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'south',
     'djcelery',
     'gunicorn',
     'folivora',
