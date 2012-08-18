f4eae1d0db74829419f420b5d0fe74c93e0bab20
Christopher Grebs <cg@webshox.org>
2012-08-18 09:18:26 +0200

    started to unittest changelog syncing

--- folivora/tests.py
+++ folivora/tests.py
@@ -1,8 +1,19 @@
 import pytz
+import mock
 from datetime import datetime
 from django.test import TestCase
 from django.utils.timezone import make_aware
 from folivora.models import Package, PackageVersion
+from folivora import tasks
+
+
+class CheesyMock(object):
+
+    def get_package_list(self):
+        return ['pmxbot']
+
+    def get_changelog(self, hours, force=False):
+        return [['pmxbot', '1101.8.1', 1345259834, 'new release']]
 
 
 class TestPackageModel(TestCase):
@@ -33,3 +44,15 @@ class TestPackageVersionModel(TestCase):
         self.assertEqual(vers.version, '0.14.6')
         self.assertEqual(vers.release_date,
                          make_aware(datetime(2012, 7, 26, 23, 51, 18), pytz.UTC))
+
+
+class TestChangelogSync(TestCase):
+
+    @mock.patch('folivora.tasks.CheeseShop', CheesyMock)
+    def test_new_release_sync(self):
+        result = tasks.sync_with_changelog.apply(throw=True)
+        self.assertTrue(result.successful())
+        pkg = Package.objects.get(name='pmxbot')
+        self.assertEqual(pkg.name, 'pmxbot')
+        self.assertEqual(pkg.provider, 'pypi')
+        self.assertEqual(pkg.versions.count(), 1)
