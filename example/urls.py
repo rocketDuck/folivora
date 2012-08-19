2f1e2092f0d800c6f306739579ab7b5c3d10c6da
Oskar Hahn <mail@oshahn.de>
2012-08-19 19:54:17 +0200

    Insert some account default templates

--- example/urls.py
+++ example/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url('^accounts/', include('django.contrib.auth.urls')),
     url('^', include('folivora.urls')),
 )
