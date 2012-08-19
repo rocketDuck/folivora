d613239343f14eff2c276288f95ff40b52a8f24f
Florian Apolloner <florian@apolloner.eu>
2012-08-19 16:59:49 +0200

    add resync to extra/sync.py

--- extra/sync.py
+++ extra/sync.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
+import sys
 import urlparse
 import datetime
 import logging
 import time
 import pytz
-from django.utils.timezone import make_aware
-from folivora.models import Package, PackageVersion
+from django.utils import timezone
+from folivora.models import Package, SyncState
 from folivora.utils.pypi import CheeseShop, DEFAULT_SERVER
 
 
@@ -14,15 +15,22 @@ SERVER = CheeseShop(DEFAULT_SERVER)
 
 
 def sync():
+    if ('delete_all' in sys.argv and
+        raw_input('sure (this resets everything)? [y/n]: ') == 'y'):
+            Package.objects.all().delete()
+
+    state, created = SyncState.objects.get_or_create(
+        type=SyncState.CHANGELOG,
+        defaults={'last_sync': timezone.now()})
+
     print 'Query package list'
     package_names = SERVER.get_package_list()
 
     packages = []
     for name in package_names:
-        #print "Query and store %s" % name
         url = urlparse.urljoin(DEFAULT_SERVER, name)
         packages.append(Package(name=name, url=url, provider='pypi'))
-    print "sync all packages to disk..."
+    print 'Sync all packages to db...'
     Package.objects.bulk_create(packages)
 
 
